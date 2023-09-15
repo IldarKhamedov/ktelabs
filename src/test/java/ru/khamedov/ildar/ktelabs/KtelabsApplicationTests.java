9dd1eded1917a2fdecd545b31364ccd5c4f4d8a4
ilkham13 <ildarhamedov@gmail.com>
2023-09-15 16:49:42 +0300

    create project

---
+++ src/test/java/ru/khamedov/ildar/ktelabs/KtelabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khamedov.ildar.ktelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtelabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
