c2989b24c46843f62cf7953fcdd1d6dc695b2339
ilkham13 <ildarhamedov@gmail.com>
2023-09-19 15:31:50 +0300

    soap2

---
+++ src/main/java/ru/khamedov/ildar/ktelabs/config/WebServiceConfig.java
@@ -0,0 +1,50 @@
+package ru.khamedov.ildar.ktelabs.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+import ru.khamedov.ildar.ktelabs.util.Constant;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    private static final String URL_MAPPINGS = "/ws/*";
+
+    private static final String LOCATION_URI = "/ws";
+
+    private static final String PORT_TYPE_NAME = "SchedulePort";
+
+    private static final String CLASS_PATH_RESOURCE = "schedule.xsd";
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, URL_MAPPINGS);
+    }
+
+    @Bean(name = "schedule")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName(PORT_TYPE_NAME);
+        wsdl11Definition.setLocationUri(LOCATION_URI);
+        wsdl11Definition.setTargetNamespace(Constant.NAMESPACE_URI);
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource(CLASS_PATH_RESOURCE));
+    }
+}
